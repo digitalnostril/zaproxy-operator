ba1b6aab33d47720b85f8e7a32b3282e0e9eb1e5
explsd <trevor@digitalnostril.com>
2024-11-03 12:45:25 -0600

    Add test for ConfigMap creation

--- controllers/zaproxy_controller_test.go
+++ controllers/zaproxy_controller_test.go
@@ -2,11 +2,15 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"time"
 
 	zaproxyorgv1alpha1 "github.com/digitalnostril/zaproxy-operator/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v2"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -15,8 +19,7 @@ import (
 var _ = Describe("ZAProxy controller", func() {
 
 	const (
-		ZAProxyName      = "test-zaproxy"
-		ZAProxyNamespace = "default"
+		ZAProxyName = "test-zaproxy"
 
 		timeout  = time.Second * 10
 		duration = time.Second * 10
@@ -24,115 +27,25 @@ var _ = Describe("ZAProxy controller", func() {
 	)
 
 	Context("When creating a ZAProxy", func() {
-		It("Should get created", func() {
-			By("By creating a new ZAProxy")
-			ctx := context.Background()
-			zaproxy := &zaproxyorgv1alpha1.ZAProxy{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "zaproxy.org/v1alpha1",
-					Kind:       "ZAPRoxy",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      ZAProxyName,
-					Namespace: ZAProxyNamespace,
-				},
-				Spec: zaproxyorgv1alpha1.ZAProxySpec{
-					StorageClassName: "standard",
-					Automation: zaproxyorgv1alpha1.Automation{
-						Plan: runtime.RawExtension{
-							Raw: []byte(`{
-								"plan": {
-									"env": {
-										"contexts": [
-											{
-												"name": "Default Context",
-												"urls": ["https://blog.digitalnostril.com"],
-												"includePaths": [],
-												"excludePaths": [],
-												"authentication": {
-													"parameters": {},
-													"verification": {
-														"method": "response",
-														"pollFrequency": 60,
-														"pollUnits": "requests"
-													}
-												},
-												"sessionManagement": {
-													"method": "cookie",
-													"parameters": {}
-												},
-												"technology": {
-													"exclude": []
-												}
-											}
-										],
-										"parameters": {
-											"failOnError": true,
-											"failOnWarning": false,
-											"progressToStdout": true
-										},
-										"vars": {}
-									},
-									"jobs": [
-										{
-											"parameters": {
-												"scanOnlyInScope": true,
-												"enableTags": false,
-												"disableAllRules": false
-											},
-											"rules": [],
-											"name": "passiveScan-config",
-											"type": "passiveScan-config"
-										},
-										{
-											"parameters": {},
-											"name": "spider",
-											"type": "spider",
-											"tests": [
-												{
-													"onFail": "INFO",
-													"statistic": "automation.spider.urls.added",
-													"site": "",
-													"operator": ">=",
-													"value": 100,
-													"name": "At least 100 URLs found",
-													"type": "stats"
-												}
-											]
-										},
-										{
-											"parameters": {},
-											"name": "passiveScan-wait",
-											"type": "passiveScan-wait"
-										},
-										{
-											"parameters": {
-												"template": "risk-confidence-html",
-												"reportDir": "/zap/reports",
-												"reportTitle": "ZAP Scanning Report",
-												"reportDescription": ""
-											},
-											"name": "report",
-											"type": "report"
-										},
-										{
-											"parameters": {
-												"time": "600",
-												"fileName": ""
-											},
-											"name": "delay",
-											"type": "delay"
-										}
-									]
-								}
-							}`),
-						},
-					},
-				},
-			}
-			Expect(k8sClient.Create(ctx, zaproxy)).To(Succeed())
+		var (
+			ctx       context.Context
+			namespace string
+			zaproxy   *zaproxyorgv1alpha1.ZAProxy
+		)
 
-			zaproxyLookupKey := types.NamespacedName{Name: ZAProxyName, Namespace: ZAProxyNamespace}
+		BeforeEach(func() {
+			ctx = context.Background()
+			namespace = fmt.Sprintf("test-namespace-%d-%d", time.Now().UnixNano(), GinkgoParallelProcess())
+
+			ns := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
+			Expect(k8sClient.Create(ctx, ns)).To(Succeed())
+
+			zaproxy = newZAProxy(ZAProxyName, namespace)
+			Expect(k8sClient.Create(ctx, zaproxy)).To(Succeed())
+		})
+
+		It("Should get created", func() {
+			zaproxyLookupKey := types.NamespacedName{Name: ZAProxyName, Namespace: namespace}
 			createdZAProxy := &zaproxyorgv1alpha1.ZAProxy{}
 
 			Eventually(func(g Gomega) {
@@ -140,5 +53,132 @@ var _ = Describe("ZAProxy controller", func() {
 			}, timeout, interval).Should(Succeed())
 			Expect(createdZAProxy.Spec.StorageClassName).To(Equal("standard"))
 		})
+
+		It("Should create a ConfigMap", func() {
+			var plan map[string]interface{}
+			json.Unmarshal(zaproxy.Spec.Automation.Plan.Raw, &plan)
+			Expect(zaproxy).NotTo(BeNil())
+			Expect(zaproxy.Spec.Automation.Plan.Raw).NotTo(BeEmpty())
+
+			planStr, err := yaml.Marshal(plan)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func(g Gomega) {
+				cm := &v1.ConfigMap{}
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: ZAProxyName + "-config", Namespace: namespace}, cm)
+				g.Expect(err).To(Succeed())
+
+				expectedData := map[string]string{
+					"af-plan.yaml": string(planStr),
+				}
+				g.Expect(cm.Data).To(Equal(expectedData))
+			}, timeout, interval).Should(Succeed())
+		})
 	})
 })
+
+func newZAProxy(name, namespace string) *zaproxyorgv1alpha1.ZAProxy {
+	return &zaproxyorgv1alpha1.ZAProxy{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "zaproxy.org/v1alpha1",
+			Kind:       "ZAPRoxy",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: zaproxyorgv1alpha1.ZAProxySpec{
+			StorageClassName: "standard",
+			Automation: zaproxyorgv1alpha1.Automation{
+				Plan: runtime.RawExtension{
+					Raw: []byte(`{
+						"plan": {
+							"env": {
+								"contexts": [
+									{
+										"name": "Default Context",
+										"urls": ["https://blog.digitalnostril.com"],
+										"includePaths": [],
+										"excludePaths": [],
+										"authentication": {
+											"parameters": {},
+											"verification": {
+												"method": "response",
+												"pollFrequency": 60,
+												"pollUnits": "requests"
+											}
+										},
+										"sessionManagement": {
+											"method": "cookie",
+											"parameters": {}
+										},
+										"technology": {
+											"exclude": []
+										}
+									}
+								],
+								"parameters": {
+									"failOnError": true,
+									"failOnWarning": false,
+									"progressToStdout": true
+								},
+								"vars": {}
+							},
+							"jobs": [
+								{
+									"parameters": {
+										"scanOnlyInScope": true,
+										"enableTags": false,
+										"disableAllRules": false
+									},
+									"rules": [],
+									"name": "passiveScan-config",
+									"type": "passiveScan-config"
+								},
+								{
+									"parameters": {},
+									"name": "spider",
+									"type": "spider",
+									"tests": [
+										{
+											"onFail": "INFO",
+											"statistic": "automation.spider.urls.added",
+											"site": "",
+											"operator": ">=",
+											"value": 100,
+											"name": "At least 100 URLs found",
+											"type": "stats"
+										}
+									]
+								},
+								{
+									"parameters": {},
+									"name": "passiveScan-wait",
+									"type": "passiveScan-wait"
+								},
+								{
+									"parameters": {
+										"template": "risk-confidence-html",
+										"reportDir": "/zap/reports",
+										"reportTitle": "ZAP Scanning Report",
+										"reportDescription": ""
+									},
+									"name": "report",
+									"type": "report"
+								},
+								{
+									"parameters": {
+										"time": "600",
+										"fileName": ""
+									},
+									"name": "delay",
+									"type": "delay"
+								}
+							]
+						}
+					}`),
+				},
+			},
+		},
+	}
+}
